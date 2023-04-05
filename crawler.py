e211d0919785574f32ea570b3b792d82177b81ee
songchez <tama4840@gmail.com>
2023-04-05 14:37:25 +0900

    venv

--- crawler.py
+++ crawler.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 #아래부터 찾고 싶은데로 바꿔!
-REGION = "원주시--강원도"
-CHECKIN = "2023-04-01"
-CHECKOUT = "2023-04-02"
+REGION = "영월역앞--영월군"
+CHECKIN = "2023-05-02"
+CHECKOUT = "2023-05-03"
 FIRST_URL = f'https://www.airbnb.co.kr/s/{REGION}/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&price_filter_input_type=0&price_filter_num_nights=1&channel=EXPLORE&date_picker_type=calendar&checkin={CHECKIN}&checkout={CHECKOUT}&source=structured_search_input_header&search_type=search_query&zoom_level=8&adults=3&query=%EA%B2%BD%EC%83%81%EB%B6%81%EB%8F%84&place_id=ChIJvRgZer-kYDURI9xMx5ppVsY&federated_search_session_id=9a8eeae2-35fc-40af-a82a-27180d726ca8&pagination_search=true&cursor=eyJzZWN0aW9uX29mZnNldCI6MCwiaXRlbXNfb2Zmc2V0IjowLCJ2ZXJzaW9uIjoxfQ%3D%3D'
 URL= ""
 data = []
